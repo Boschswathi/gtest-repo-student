f5b31094322d930092050f6b110a6154435a7805
Boschswathi <167760969+Boschswathi@users.noreply.github.com>
2024-04-22 16:06:54 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -16,7 +16,7 @@ TEST(StringCalculatorTestSuite,Given2numbers){
     //Arrangee
     StringCalculator objUnderTest;
     string input="1,2";
-    int expectedValue=3;
+    int expectedValue=4;
     //Act
    int actualValue=  objUnderTest.Add(input);
     //Assert
